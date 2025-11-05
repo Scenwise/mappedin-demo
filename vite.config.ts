6082733a098cb6ec2e695fa0561781fec2c9f10c
Radoslav <radoslav.sozonov@gmail.com>
2025-11-05 21:39:49 +0200

    Add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
