f03b472372c86e81d6e4f3ed6f4f46f3c3c3d536
Radoslav <radoslav.sozonov@gmail.com>
2025-11-07 12:10:52 +0200

    Change file

--- netlify/functions/ssr.cjs
+++ netlify/functions/ssr.cjs
@@ -1,9 +1,4 @@
-import path from "path";
-import { createRequestHandler } from "react-router";
-import { fileURLToPath } from "url";
-
-// path to your server build
-const { createRequestHandler } = require("react-router");
 const build = require("./server.cjs");
 
-exports.handler = createRequestHandler(build);
+exports.handler = build.default;
+
