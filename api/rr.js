11764dc162dea8760a57a578eb56f14ce1e24089
Radoslav <radoslav.sozonov@gmail.com>
2025-11-07 13:10:55 +0200

    Change files

--- api/rr.js
+++ api/rr.js
@@ -1,11 +1,13 @@
+export const config = {
+  runtime: "edge"
+};
+
 import build from "../build/server/index.js";
 
 export default async function handler(request) {
-  const url = new URL(request.url);
-
   return build.entry.module.default({
     request,
     params: {},
-    context: {},
+    context: {}
   });
 }
